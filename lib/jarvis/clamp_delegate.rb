24fe01c1f4bf998ed2a93386c58be589a83eca9c
Jordan Sissel <jls@semicomplete.com>
2015-11-11 12:32:30 -0800

    Allow dynamic flag passing from Lita environment to Clamp
    This lets us still use CLI/Chat the same. We can now map Lita data to
    Clamp flags. For example, having a user profile that has a setting such
    as `git email` that we want to use to pass to the merge command's
    --committer flag.

--- lib/jarvis/clamp_delegate.rb
+++ lib/jarvis/clamp_delegate.rb
@@ -13,18 +13,41 @@ module Jarvis module ClampDelegate
   # The 'foo' command, when invoked via the Lita bot, will pass the whole
   # message body into the Foo clamp command as if it were run via the command
   # line.
-  def self.delegate(command_class)
+  def self.delegate(command_class, options)
     if !command_class.ancestors.include?(Clamp::Command)
       raise ArgumentError, "Cannot delegate to `#{command_class}` because it is not a subclass of Clamp::Command"
     end
-    callback(command_class)
+    callback(command_class, options)
   end
 
   private
-  def self.callback(command_class)
+  def self.callback(command_class, options)
+    if options.include?(:flags) 
+      if !options[:flags].is_a?(Hash) 
+        raise ArgumentError, ":flags option to #{command_class} must be a Hash of { \"--string-flag\" => lambda { |x| ... } }"
+      end
+      if !options[:flags].all? { |k,v| k.is_a?(String) && v.is_a?(Proc) }
+        raise ArgumentError, ":flags option to #{command_class} must be a Hash of { \"--string-flag\" => lambda { |x| ... } }"
+      end
+      if !options[:flags].all? { |k,_| k =~ /^--/ }
+        raise ArgumentError, ":flags keys must all be string starting with '--', like { \"--example\" => lambda { |x| ... } }"
+      end
+    end
+
     lambda do |request|
       name, *args = Shellwords.shellsplit(request.message.body)
       cmd = command_class.new(name)
+
+      if options.include?(:flags)
+        # prepend flags to the args
+        # The `:flags` value is expected to be a Hash of "--flag-name" => lambda { |request| ... }
+        # The idea is that there can be automatic flags based on the
+        # circumstances of the request.
+        options[:flags].each do |key, value_proc|
+          value = value_proc.call(request)
+          args.unshift(key, value) if value
+        end
+      end
 
       inject_methods(cmd, request)
 
--- lib/lita/handlers/jarvis.rb
+++ lib/lita/handlers/jarvis.rb
@@ -9,7 +9,9 @@ module Lita
       extend ::Jarvis::Mixins::FancyRoute
 
       fancy_route("restart", ::Jarvis::Command::Bounce, :command => true, :pool => ::Jarvis::WorkPool::ADMINISTRATIVE)
-      fancy_route("merge", ::Jarvis::Command::Merge, :command => true)
+      fancy_route("merge", ::Jarvis::Command::Merge, :command => true, :flags => {
+        "--committer" => ->(request) { request.user.metadata["git-email"] || raise(::Jarvis::UserProfileError, "Missing user setting `git-email` for user #{request.user.name}") }
+      })
       fancy_route("cla", ::Jarvis::Command::CLA, :command => true)
 
       Lita.register_handler(self)
