fc38e9b799eda3b6ae5562b6496053b83ed291b7
Jordan Sissel <jls@semicomplete.com>
2015-11-11 17:59:39 -0800

    Pass --cla-url to the cla command based on the lita config value config.handlers.jarvis.cla_url

--- lib/lita/handlers/jarvis.rb
+++ lib/lita/handlers/jarvis.rb
@@ -7,13 +7,17 @@ module Lita
   module Handlers
     class Jarvis < Handler
       extend ::Jarvis::Mixins::FancyRoute
+      config :cla_url
+      config :organization
 
       fancy_route("restart", ::Jarvis::Command::Bounce, :command => true, :pool => ::Jarvis::WorkPool::ADMINISTRATIVE)
       fancy_route("merge", ::Jarvis::Command::Merge, :command => true, :flags => {
         "--committer-email" => ->(request) { request.user.metadata["git-email"] || raise(::Jarvis::UserProfileError, "Missing user setting `git-email` for user #{request.user.name}") },
         "--committer-name" => ->(request) { request.user.name },
       })
-      fancy_route("cla", ::Jarvis::Command::CLA, :command => true)
+      fancy_route("cla", ::Jarvis::Command::CLA, :command => true, :flags => {
+        "--cla-url" => ->(_) { config.find { |c| c.name == :cla_url }.value },
+      })
 
       Lita.register_handler(self)
     end
