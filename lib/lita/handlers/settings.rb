af1654db3bc509bd84aea575bd15a4e67eedabc8
Jordan Sissel <jls@semicomplete.com>
2015-11-15 13:23:25 -0800

    Restructure user profile commands
    It was "set user ..." now it is "user set ..."
    
    I think this will make more sense since we can logically scope commands
    under "user" and later "project" etc.

--- lib/lita/handlers/settings.rb
+++ lib/lita/handlers/settings.rb
@@ -1,20 +1,37 @@
 module Lita module Handlers class Settings < Handler
   # set user some-key some-value
-  route(/^set\s+user\s+(\S+)\s+(.*)\s*$/, :command => true, :help => {"set user <key> <value>" => t("set user")}) do |request|
+  route(/^user\s+get\s+(\S+)\s+(.*)\s*$/, :command => true, :help => {"user set <key> <value>" => t("help.user set")}) do |request|
     _, key, value, *_ = request.match_data.to_a
     request.user.metadata[key] = value
     request.user.save
     request.reply(t("user profile updated", :user => request.user.name, :key => key))
   end
 
-  # get user some-key
-  route(/^get\s+user\s+(\S+)\s*$/, :command => true, :help => {"get user <key>" => t("get user")}) do |request|
+  # user get some-key
+  route(/^user\s+get\s+(\S+)\s*$/, :command => true, :help => {"user get <key>" => t("help.user get")}) do |request|
     _, key, *_ = request.match_data.to_a
     value = request.user.metadata[key]
     if value
-      request.reply(t("user profile get", :user => request.user.name, :key => key, :value => value))
+      request.reply(t("response.user profile get", :user => request.user.name, :key => key, :value => value))
     else
-      request.reply(t("user profile key not set", :user => request.user.name, :key => key))
+      request.reply(t("response.user profile key not set", :user => request.user.name, :key => key))
+    end
+  end
+ 
+  # clear user some-key
+  route(/^user\s+clear\s+(\S+)\s*$/, :command => true, :help => {"user clear <key>" => t("help.user clear")}) do |request|
+    _, key, *_ = request.match_data.to_a
+    request.user.metadata.delete(key)
+    request.user.save
+    request.reply(t("response.user profile clear", :user => request.user.name, :key => key))
+  end
+
+  route(/^user\s+get\s*$/, :command => true, :help => {"user get" => t("help.user get")}) do |request|
+    text = request.user.metadata.collect { |k,v| "#{k} = #{v}" }.join("\n")
+    if text.empty?
+      request.reply(t("response.empty user profile"))
+    else
+      request.reply(text)
     end
   end
 
