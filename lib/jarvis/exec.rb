dd0d60f05fac67cb01702a96977b50e36b8fba60
Jordan Sissel <jls@semicomplete.com>
2015-11-19 01:14:15 +0000

    Actually run the commands

--- lib/jarvis/exec.rb
+++ lib/jarvis/exec.rb
@@ -6,10 +6,10 @@ module Jarvis
 
   def self.execute(args, logger)
     logger.info("Running command", :args => args)
-    #pid, stdin, stdout, stderr = Open4::popen4(*args)
-    #stdin.close
-    #logger.pipe(stdout => :info, stderr => :error)
-    #_, status = Process::waitpid2(pid)
-    #raise SubprocessFailure, "subprocess failed with code #{status.exitstatus}" unless status.success?
+    pid, stdin, stdout, stderr = Open4::popen4(*args)
+    stdin.close
+    logger.pipe(stdout => :info, stderr => :error)
+    _, status = Process::waitpid2(pid)
+    raise SubprocessFailure, "subprocess failed with code #{status.exitstatus}" unless status.success?
   end
 end
